b4917040360d015b6162c9fbff8c773f84b10044
sahandilshan <sahandilshan222@gmail.com>
2022-01-11 15:08:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ echo "Github Action: $GITHUB_ACTION"
 echo "Github Action Path: $GITHUB_ACTION_PATH"
 echo "GitHub Token: $GITHUB_TOKEN"
 echo "GitHub Actor: $GITHUB_ACTOR"
+echo "GitHub Event Path $GITHUB_EVENT_PATH"
 time=$(date)
 echo ::set-output name=time::$time
