79de2dca49e363ade0751507f2abbd6ce76dc4b3
sahandilshan <sahandilshan222@gmail.com>
2022-01-11 16:19:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,13 @@ echo "GitHub Actor: $GITHUB_ACTOR"
 echo "GitHub Event Path $GITHUB_EVENT_PATH"
 ISSUE_ID=$(jq -r '.issue.id' < "$GITHUB_EVENT_PATH")
 echo "Issue Id: $ISSUE_ID"
+echo "Owner: $GITHUB_REPOSITORY"
+
+PROJECT_URL=$2
+PROJECT_JSON=$(curl -i -u sahandilshan:$GITHUB_TOKEN --location --request GET 'https://api.github.com/repos/sahandilshan/sample-repo/projects' \
+--header 'Accept: application/vnd.github.v3+json')
+echo "$PROJECT_JSON"
+
+
 time=$(date)
 echo ::set-output name=time::$time
